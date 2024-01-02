e993673cf86945b81b6df92cd333a85c8713ec2d
davidissa99 <105718368+davidissa99@users.noreply.github.com>
2024-01-02 15:50:35 +0000

    Add files via upload

--- music-genre-recognition-webapp.py
+++ music-genre-recognition-webapp.py
@@ -7,7 +7,7 @@ st.write("## Know the genre of your favorite musics!")
 st.sidebar.write("## Upload youtube music link")
 #st.sidebar.image("images_webapp/icons8-youtube.gif")
 
-youtube_url = st.sidebar.text_input(,
+youtube_url = st.sidebar.text_input('',
                                     "Paste here the music youtube link",
                                     key="youtube_url",
                                     )
