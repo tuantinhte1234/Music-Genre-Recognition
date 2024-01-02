8bc1a9bfa90837e44f241444b8fdf68d00f5d9c6
davidissa99 <105718368+davidissa99@users.noreply.github.com>
2024-01-02 15:40:53 +0000

    Add files via upload

--- music-genre-recognition-webapp.py
+++ music-genre-recognition-webapp.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide", page_title="Music Genre Recognition App")
 st.write("## Know the genre of your favorite musics!")
 
 st.sidebar.write("## Upload youtube music link")
-st.image("images_webapp/icons8-youtube.gif")
+st.sidebar.image("images_webapp/icons8-youtube.gif")
 
 
 
