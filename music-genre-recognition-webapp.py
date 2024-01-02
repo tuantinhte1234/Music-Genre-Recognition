799c510b0345aff23684ec531d6876ced3bb50e2
davidissa99 <105718368+davidissa99@users.noreply.github.com>
2024-01-02 15:32:22 +0000

    Add files via upload

--- music-genre-recognition-webapp.py
+++ music-genre-recognition-webapp.py
@@ -2,6 +2,11 @@ import streamlit as st
 import base64
 
 st.set_page_config(layout="wide", page_title="Music Genre Recognition App")
-st.write("## Know the music genre of your favorite musics!")
+st.write("## Know the genre of your favorite musics!")
 
-st.sidebar.write("## Upload youtube music link :youtube:")
+st.sidebar.write("## Upload youtube music link")
+#### <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/youtube-play.png" alt="youtube-play"/></i>
+
+
+
+#<a  href="https://icons8.com/icon/37326/youtube">YouTube</a> icon by <a href="https://icons8.com">Icons8</a>
