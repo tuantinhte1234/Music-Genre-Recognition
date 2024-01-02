8982da9feb5b7fd0a686253c6860c76d80cfdbf4
davidissa99 <105718368+davidissa99@users.noreply.github.com>
2024-01-02 14:16:28 +0000

    Add files via upload

---
+++ Music Genre Recognition WebApp.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import base64
+
+st.set_page_config(layout="wide", page_title="Music Genre Recognition App")
+st.write("## Know the music genre of your favorite musics!")
+
+st.sidebar.write("## Upload youtube music link :youtube:")
