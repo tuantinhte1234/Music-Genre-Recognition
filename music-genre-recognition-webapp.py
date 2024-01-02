72ba70f4fd075248acd136e88e884db6ab0b9f6d
davidissa99 <105718368+davidissa99@users.noreply.github.com>
2024-01-02 15:46:38 +0000

    Add files via upload

--- music-genre-recognition-webapp.py
+++ music-genre-recognition-webapp.py
@@ -7,6 +7,11 @@ st.write("## Know the genre of your favorite musics!")
 st.sidebar.write("## Upload youtube music link")
 st.sidebar.image("images_webapp/icons8-youtube.gif")
 
+st.sidebar.text_input("",
+                      "Please enter youtube link",
+                      key="youtube_url",
+                      )
+
 
 
 #<a  href="https://icons8.com/icon/37326/youtube">YouTube</a> icon by <a href="https://icons8.com">Icons8</a>
