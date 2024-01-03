cca0b896f2e5c554271f45afc869d0b3cca10467
davidissa99 <105718368+davidissa99@users.noreply.github.com>
2024-01-03 18:51:21 +0000

    Add files via upload

--- music-genre-recognition-webapp.py
+++ music-genre-recognition-webapp.py
@@ -5,38 +5,62 @@ import os
 import subprocess 
 import librosa
 import tempfile 
+from pydub import AudioSegment
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import load_img,img_to_array
 
+#begin seeting up webapp title
 st.set_page_config(layout="wide", page_title="Music Genre Recognition App")
 st.write("## Know the genre of your favorite musics!")
 
-st.sidebar.write("## Upload the mp3 file of a music of your choice:")
+#define social medias
+col1,col2 = st.sidebar.columns(2)
 
+linked_in = '''[![LinkedIn](https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/120px-LinkedIn_Logo.svg.png)](https://www.linkedin.com/in/david-iss%C3%A1/)'''
+col1.markdown(linked_in, unsafe_allow_html=True)
+github = '''[![LinkedIn](https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/GitHub_logo_2013.svg/120px-GitHub_logo_2013.svg.png)](https://github.com/davidissa99)'''
+col2.markdown(github, unsafe_allow_html=True)
+
+
+# upload mp3 file and visualize it
+st.sidebar.write("## Upload the mp3 file of a music of your choice:")
 mp3_file = st.sidebar.file_uploader("Upload an audio file", type=["mp3"])
 
-#st.sidebar.image("images_webapp/icons8-youtube.gif")
 
-#youtube_url = st.sidebar.text_input('',
-#                                placeholder="Paste here the url...",
-#                                key="youtube_url",
-#                                label_visibility="collapsed",
-#                                )
+#define function to convert mp3 to wav format
+def convert_mp3_to_wav(music_file):  
+    sound = AudioSegment.from_mp3(music_file)
+    sound.export("music_file.wav",format="wav")
+  
+#define funciton to produce and save mel spectogram
+def create_melspectrogram(wav_file):  
+    y,sr = librosa.load(wav_file)  
+    mel_spec = librosa.power_to_db(librosa.feature.melspectrogram(y=y,sr=sr))    
+    plt.figure(figsize=(10, 5))
+    plt.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[])
+    librosa.display.specshow(mel_spec, x_axis="time", y_axis='mel', sr=sr)
+    plt.margins(0)
+    plt.savefig('melspectrogram.png')
 
-#def extract_youtube_mp3(url):
-#    video = pytube.YouTube(url).streams.filter(only_audio=True).first() 
-#    mp4_audio = video.download() 
-#    base, ext = os.path.splitext(mp4_audio) 
-#    mp3_audio = base + '.mp3'
-#    os.rename(mp4_audio, mp3_audio) 
-#    return mp3_audio
+#define function to predict music genre based on mel spectogram
+def predict(image_data, model):   
+    image = img_to_array(image_data)   
+    image = np.reshape(image,(1,100,200,4))   
+    prediction = model.predict(image/255)   
+    prediction = prediction.reshape((9,))     
+    class_label = np.argmax(prediction)     
+    return class_label, prediction
 
-def mp3_to_wav(mp3_audio):
-    subprocess.call(['ffmpeg', '-i', mp3_audio, os.path.splitext(mp3_audio)[0] + '.wav'])
-    wav_audio, sr = librosa.load(os.path.splitext(mp3_audio)[0] + '.wav')
-    return wav_audio, sr    
+class_labels = ['blues', 'classical', 'country', 'disco', 'hiphop', 'metal', 'pop', 'reggae', 'rock']
+    
 
-if mp3_file is not None: 
-    with tempfile.mkstemp(suffix="mp3") as temp:
-        temp.write(mp3_file.getvalue())
-        temp.seek(0)
-        wav_audio, sr = mp3_to_wav(temp)
-        st.sidebar.audio(wav_audio, sample_rate=sr)
+#convert mp3 file to wav and listen to it
+if mp3_file is not None:  
+  st.sidebar.audio(mp3_file,"audio/mp3")  
+  convert_mp3_to_wav(mp3_file)
+  create_melspectrogram("music_file.wav")
+  image_data = load_img('melspectrogram.png', color_mode='rgba', target_size=(100,200))   
+  st.sidebar.image('melspectrogram.png')
+        
+        
+    
