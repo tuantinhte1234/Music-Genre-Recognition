e445180cad46e1c499fd80934aae5a9a5ccbaffb
davidissa99 <105718368+davidissa99@users.noreply.github.com>
2024-01-03 14:09:44 +0000

    Add files via upload

--- music-genre-recognition-webapp.py
+++ music-genre-recognition-webapp.py
@@ -23,12 +23,12 @@ def extract_youtube_mp3(url):
     mp4_audio = video.download() 
     base, ext = os.path.splitext(mp4_audio) 
     mp3_audio = base + '.mp3'
-    os.rename(out_file, mp3_audio) 
+    os.rename(mp4_audio, mp3_audio) 
     return mp3_audio
 
 def mp3_to_wav(mp3_audio):
     subprocess.call(['ffmpeg', '-i', mp3_audio, os.path.splitext(mp3_audio)[0] + '.wav'])
-    wav_audio, sr = librosa.load('converted_to_wav_file.wav')
+    wav_audio, sr = librosa.load(os.path.splitext(mp3_audio)[0] + '.wav')
     return wav_audio, sr
 
 if youtube_url != "": 
