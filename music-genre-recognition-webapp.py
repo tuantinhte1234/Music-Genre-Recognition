5cb118e8130f623a68193980c0c238f95da677f5
davidissa99 <105718368+davidissa99@users.noreply.github.com>
2024-01-03 17:08:26 +0000

    Add files via upload

--- music-genre-recognition-webapp.py
+++ music-genre-recognition-webapp.py
@@ -8,30 +8,30 @@ import librosa
 st.set_page_config(layout="wide", page_title="Music Genre Recognition App")
 st.write("## Know the genre of your favorite musics!")
 
-st.sidebar.write("## Upload the youtube url of a music of your choice:")
+st.sidebar.write("## Upload the mp3 file of a music of your choice:")
 #st.sidebar.image("images_webapp/icons8-youtube.gif")
 
-youtube_url = st.sidebar.text_input('',
-                                    placeholder="Paste here the url...",
-                                    key="youtube_url",
-                                    label_visibility="collapsed",
-                                    )
+#youtube_url = st.sidebar.text_input('',
+#                                placeholder="Paste here the url...",
+#                                key="youtube_url",
+#                                label_visibility="collapsed",
+#                                )
 
+#def extract_youtube_mp3(url):
+#    video = pytube.YouTube(url).streams.filter(only_audio=True).first() 
+#    mp4_audio = video.download() 
+#    base, ext = os.path.splitext(mp4_audio) 
+#    mp3_audio = base + '.mp3'
+#    os.rename(mp4_audio, mp3_audio) 
+#    return mp3_audio
 
-def extract_youtube_mp3(url):
-    video = pytube.YouTube(url).streams.filter(only_audio=True).first() 
-    mp4_audio = video.download() 
-    base, ext = os.path.splitext(mp4_audio) 
-    mp3_audio = base + '.mp3'
-    os.rename(mp4_audio, mp3_audio) 
-    return mp3_audio
+mp3_file = st.file_uploader("Choose a file")
 
 def mp3_to_wav(mp3_audio):
     subprocess.call(['ffmpeg', '-i', mp3_audio, os.path.splitext(mp3_audio)[0] + '.wav'])
     wav_audio, sr = librosa.load(os.path.splitext(mp3_audio)[0] + '.wav')
-    return wav_audio, sr
+    return wav_audio, sr    
 
-if youtube_url != "": 
-    mp3_audio = extract_youtube_mp3(youtube_url)
+if mp3_file is not None: 
     wav_audio, sr = mp3_to_wav(mp3_audio)
     st.sidebar.audio(wav_audio, sample_rate=sr)
