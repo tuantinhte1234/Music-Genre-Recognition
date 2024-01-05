0a4ac94419f06fa635743789e063cf74046a61c6
davidissa99 <105718368+davidissa99@users.noreply.github.com>
2024-01-05 14:49:01 +0000

    Add files via upload

--- music-genre-recognition-webapp.py
+++ music-genre-recognition-webapp.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.colors import Normalize
 import tensorflow as tf
+from statistics import mode
 from tensorflow import keras
 from keras import regularizers
 from keras.preprocessing.image import load_img,img_to_array
@@ -107,7 +108,7 @@ def predict(image_data, model):
     return class_label, prediction
 
 
-class_labels = ['blues', 'classical', 'country', 'disco', 'pop', 'hiphop', 'jazz', 'metal', 'reggae', 'rock']
+class_labels = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal',  'pop',  'reggae', 'rock']
     
     
 #configure bacckground
@@ -136,34 +137,39 @@ mp3_file = st.sidebar.file_uploader("Upload an audio file", type=["mp3"], label_
 if mp3_file is not None:    
   st.sidebar.write("**Play the song below if you want!**")
   st.sidebar.audio(mp3_file,"audio/mp3")
-    
-  convert_mp3_to_wav(mp3_file)
-  
-  create_melspectrogram("music_file.wav")
-  image_data = load_img('melspectrogram.png', color_mode='rgba', target_size=(100,200))   
   
   model = GenreModel(input_shape=(100,200,4),classes=10)
   model.load_weights("music_genre_recog_model.h5")
   
-  class_label, prediction = predict(image_data, model)
+  convert_mp3_to_wav(mp3_file)
+  audio_full = AudioSegment.from_wav('music_file.wav')
   
-  st.markdown("<h4 style='text-align: center; color: black;'>The genre of your song is: {} </h4>".format(class_labels[class_label]), unsafe_allow_html=True) 
+  class_labels_total = []
+  predictions_total = []
+  for w in range(int(round(len(audio_full)/3000,0))):
+      audio_3sec = audio_full[3*(w)*1000:3*(w+1)*1000]
+      audio_3sec.export(out_f = "audio_3sec.wav", format = "wav")
+      create_melspectrogram("audio_3sec.wav")
+      image_data = load_img('melspectrogram.png', color_mode='rgba', target_size=(100,200))   
+      class_label, prediction = predict(image_data, model)
+      prediction = prediction.reshape((10,)) 
+      class_labels_total.append(class_label)
+      predictions_total.append(prediction)
+  
+  class_label_final = mode(class_labels_total)
+  #color_data = [1,2,3,4,5,6,7,8,9,10]
+  #my_cmap = cm.get_cmap('Blues')
+  #my_norm = Normalize(vmin=0, vmax=10)
+  #fig,ax= plt.subplots(figsize=(10,5))
+  #ax.bar(x=class_labels,height=prediction, color=my_cmap(my_norm(color_data)))
+  #ax.spines['top'].set_visible(False)
+  #ax.spines['right'].set_visible(False)
+  #ax.spines['left'].set_visible(False)
+  #ax.spines['bottom'].set_color('#DDDDDD')
+  #ax.tick_params(bottom=False, left=False)
+  #plt.savefig("prob_distribution_genres.png",format='b png', dpi=1000, transparent=True)
+  
+  st.markdown("<h4 style='text-align: center; color: black;'>The genre of your song is: {} </h4>".format(class_labels[class_label_final]), unsafe_allow_html=True) 
   st.markdown("<h4 style='text-align: center; color: black;'></h4>", unsafe_allow_html=True) 
-
-  prediction = prediction.reshape((10,)) 
-  
-  color_data = [1,2,3,4,5,6,7,8,9,10]
-  my_cmap = cm.get_cmap('Blues')
-  my_norm = Normalize(vmin=0, vmax=10)
-  
-  fig,ax= plt.subplots(figsize=(10,5))
-  ax.bar(x=class_labels,height=prediction,
-  color=my_cmap(my_norm(color_data)))
-  ax.spines['top'].set_visible(False)
-  ax.spines['right'].set_visible(False)
-  ax.spines['left'].set_visible(False)
-  ax.spines['bottom'].set_color('#DDDDDD')
-  ax.tick_params(bottom=False, left=False)
-  plt.savefig("prob_distribution_genres.png",format='png', dpi=1000, transparent=True)
-  st.image("prob_distribution_genres.png", use_column_width=True, caption="Probability Distribution Of The Given Song Over Different Genres")
+  #st.image("prob_distribution_genres.png", use_column_width=True, caption="Probability Distribution Of The Given Song Over Different Genres")
 
